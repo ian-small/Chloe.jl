305f8abeda79a96a00d82fe8ec0c4e0319ae7a84
Ian Castleden <ian.castleden@gmail.com>
2020-03-27 13:21:38 +0800

    fix bug to ensure connection to correct server

--- bin/bgregression.sh
+++ bin/bgregression.sh
@@ -2,15 +2,16 @@
 if [ ! -d testo ]; then
     mkdir testo
 fi
-JULIA_NUM_THREADS=4 julia src/chloe_svr.jl --level=warn &
+A=tcp://127.0.0.1:9999
+JULIA_NUM_THREADS=4 julia src/chloe_svr.jl -a $A --level=warn &
 for f in $(ls testfa/*.fa)
 do
     echo "annotating $f"
-    python bin/chloe.py annotate $f -o testo
+    python bin/chloe.py annotate -a $A -o testo $f 
     bn=$(basename $f)
     o="${bn%.*}.sff"
     echo "diffing testfa/$o testo/$o"
     diff testfa/$o testo/$o
 done
-python bin/chloe.py terminate
+python bin/chloe.py terminate -a $A
 rm -rf testo
