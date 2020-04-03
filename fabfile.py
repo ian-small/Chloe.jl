fe7b5f51adc8bd868a14f62d0d6f49d484fda5bd
Ian Castleden <ian.castleden@gmail.com>
2020-04-03 20:27:44 +0800

    added stiletto update

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,19 @@
 # import sys
 import os
+from warnings import filterwarnings
 from fabric import task
 from click import style as color, echo, secho
+from cryptography.utils import CryptographyDeprecationWarning
+
+filterwarnings("ignore", category=CryptographyDeprecationWarning)
 
 
 local_dir = os.path.dirname(__file__) or "."
 remote_dir = "/var/www/websites3/annotator"
 
 HOSTS = ["ianc@croppal"]
+
+STILETTO = ["ianc@chloe-stiletto"]
 
 
 def git_uptodate(res):
@@ -30,6 +36,18 @@ def update(c):
             secho('restarting service use: "fab ping" to check restart', fg="magenta")
             # hopefully supervisor will restart (see etc/supervisor-chloe-celery.conf)
             c.run("python bin/chloe.py terminate")
+
+
+@task(hosts=STILETTO)
+def update_stiletto(c):
+    """Update code from github."""
+    with c.cd("chloe-svr"):
+        pwd = c.run("pwd", hide=True)
+        echo("pulling from: " + color(pwd.stdout.strip(), fg="yellow"))
+        result = c.run("uname -a", hide=True)
+        # ,result.failed,result.return_code,result.succeeded
+        echo(color(result.stdout.strip(), fg="green"))
+        c.run("git pull")
 
 
 @task(hosts=HOSTS)
