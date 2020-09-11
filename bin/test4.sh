6055b4015a74350ed87ebd8da893af1184f106b8
Ian Castleden <ian.castleden@gmail.com>
2020-09-11 20:21:01 +0800

    bugfix

--- bin/test4.sh
+++ bin/test4.sh
@@ -3,4 +3,4 @@ if [ ! -d testo ]; then
     mkdir testo
 fi
 tfiles="testfa/NC_020019.1.fa testfa/NC_020318.1.fa testfa/NC_020152.1.fa testfa/NC_020320.1.fa"
-exec python bin/chloe.py annotate -o testo --parallel  $tfiles
+exec python bin/chloe.py annotate -o testo --workers=4  $tfiles
