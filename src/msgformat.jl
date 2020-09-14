273a21d995e5e73107a914135803f256791ba606
Ian Castleden <ian.castleden@gmail.com>
2020-09-14 09:16:57 +0800

    add a check for trermination

--- src/msgformat.jl
+++ src/msgformat.jl
@@ -1,23 +1,33 @@
 
-# using JSON
-
-using JuliaWebAPI
+import JuliaWebAPI
 import JuliaWebAPI: juliaformat, AbstractMsgFormat, juliaformat, wireformat, JSONMsgFormat
 
 # overide JuliaWebAPI interface
 
-struct JSONMsgFormatEx <: AbstractMsgFormat
+struct TerminatingJSONMsgFormat <: AbstractMsgFormat
 end
 
 const IJSONMsgFormat = JSONMsgFormat()
 
-JuliaWebAPI.wireformat(fmt::JSONMsgFormatEx, 
+JuliaWebAPI.wireformat(fmt::TerminatingJSONMsgFormat, 
     cmd::String, args...; data...) = wireformat(IJSONMsgFormat, cmd, args... ; data...)
-JuliaWebAPI.wireformat(fmt::JSONMsgFormatEx, code::Int, 
+JuliaWebAPI.wireformat(fmt::TerminatingJSONMsgFormat, code::Int, 
     headers::Dict{String,String}, resp, id=nothing) = wireformat(IJSONMsgFormat, code, headers, resp, id)
 
 
-function JuliaWebAPI.juliaformat(fmt::JSONMsgFormatEx, msgstr)
-    @info "JSONMsgFormatEx: calling $(msgstr)"
-    juliaformat(IJSONMsgFormat, msgstr)
+function JuliaWebAPI.juliaformat(fmt::TerminatingJSONMsgFormat, msgstr)
+    ret = juliaformat(IJSONMsgFormat, msgstr)
+    if get(ret, "cmd", "") == ":exit"
+        pid = get(ret, "args", [])
+        if length(pid) != 1
+            error("wrong arguments")
+        end
+        if getpid() == pid[1]
+            ret["cmd"] = ":terminate"
+        else
+            error("exit: incorrect process")
+        end
+    end
+    return ret
 end
+
