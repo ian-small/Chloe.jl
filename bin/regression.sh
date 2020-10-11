fe62195032ad45c68fa8dcd70775bd54580dd6e1
Ian Castleden <ian.castleden@gmail.com>
2020-10-11 15:06:43 +0800

    better ui

--- bin/regression.sh
+++ bin/regression.sh
@@ -8,12 +8,13 @@ rm -rf testo/*
 O='\e[0m'
 G='\e[1;32m'
 R='\e[1;31m'
+A='\e[1;30m' # grey
 
 echo "start annotations..."
-JULIA_NUM_THREADS=8 time -p julia "$@" chloe.jl -l info annotate -o testo testfa/*.fa
+time -p julia --threads=8 "$@" chloe.jl -l info annotate -o testo testfa/*.fa
 for f in $(ls testo)
 do 
-    echo "diffing $f"
+    echo -e "diffing: $f ${A}(> means line from original)${O}"
     diff testo/$f testfa/$f
     if [ $? -eq 0 ]; then
         echo -e "$G******** test OK ***********$O"
