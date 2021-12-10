677738243a41682f803e3eb1609f971ba3815b47
Ian Small <ians@chloe.novalocal>
2021-12-10 03:05:54 +0000

    nimbus branch

--- src/globals.jl
+++ src/globals.jl
@@ -2,7 +2,7 @@ const REPO_DIR = dirname(@__FILE__)
 const DEFAULT_GSREFS = "chloe_references/gsrefs"
 const DEFAULT_NUMGSREFS = 8
 const DEFAULT_CHLOEREFS = "chloe_references/chloerefs"
-const DEFAULT_NUMCHLOEREFS = 0
+const DEFAULT_NUMCHLOEREFS = 8
 const DEFAULT_TEMPLATE = "chloe_references/templates.tsv"
 const DEFAULT_SENSITIVITY = 0.5
 
