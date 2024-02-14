7a805e58480ca99070a5cc9635ca21e617d83645
Ian Small <ian.small@uwa.edu.au>
2024-02-14 13:15:35 +0800

    Delete everything in the old chloe repo

--- bin/install_dependencies.jl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env julia
-using Pkg
-#println("Activating environment in $(pwd())")
-pkg"activate ."
-println("Installing packages..."); flush(stdout);
-pkg"instantiate"
-pkg"precompile"
-println("Done!")
--- chloe.jl
+++
@@ -1,9 +0,0 @@
-include("src/annotate_genomes.jl")
-include("src/sff2GFF3.jl")
-include("src/MMap.jl")
-include("src/chloe_cmd.jl")
-
-if abspath(PROGRAM_FILE) == @__FILE__
-    import .CmdLine: cmd_main
-    cmd_main()
-end
